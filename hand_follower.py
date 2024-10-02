1ef7250936e0c24a98c90ed9723960142f53e4a8
sungals91 <sungals911@gmail.com>
2024-10-03 00:02:43 +0900

    창 닫기를 누르거나 ESC를 누르면 종료

--- hand_follower.py
+++ hand_follower.py
@@ -8,10 +8,21 @@ arrow = load_image('hand_arrow.png')
 running = True
 x, y = 800//2, 600//2
 
+def handle_events():
+    global running
+
+    for event in get_events():
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+    pass
 
 while running:
     clear_canvas()
     background.draw(x, y)
     update_canvas()
+    handle_events()
 
 close_canvas()
